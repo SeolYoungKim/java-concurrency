5f31d69efd9125e0faa3f0624d27b9c84d817045
SeolYoungKim <nasur4da@gmail.com>
2024-02-12 23:02:31 +0900

    feat: project init

---
+++ src/main/java/thread_safety/StatelessFactorizer.java
@@ -0,0 +1,35 @@
+package thread_safety;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import java.io.IOException;
+
+public class StatelessFactorizer implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
