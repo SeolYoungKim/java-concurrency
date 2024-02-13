0c96e879951291fe7125b141e898b61808f852c5
SeolYoungKim <nasur4da@gmail.com>
2024-02-13 19:33:04 +0900

    feat: 스레드 안전한 서블릿

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,10 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
     compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- src/main/java/thread_safety/StatelessFactorizer.java
+++ src/main/java/thread_safety/StatelessFactorizer.java
@@ -6,7 +6,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import java.io.IOException;
+import java.math.BigInteger;
 
+/**
+ * 자바 병렬 프로그래밍 02장에 사용된 예제
+ */
 public class StatelessFactorizer implements Servlet {
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
@@ -20,7 +24,22 @@ public class StatelessFactorizer implements Servlet {
 
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        BigInteger i = extractFromRequest(servletRequest);
+        BigInteger[] factors = factor(i);
+        encodeIntoResponse(servletResponse, factors);
+    }
 
+    private void encodeIntoResponse(ServletResponse servletResponse, BigInteger[] factors) {
+
+    }
+
+    private BigInteger extractFromRequest(ServletRequest req) {
+        return new BigInteger("7");  // 예제 편의를 위한 코드 😅
+    }
+
+    private BigInteger[] factor(BigInteger i) {
+        // 대충 인수 분해를 하는 메서드
+        return new BigInteger[0];
     }
 
     @Override
